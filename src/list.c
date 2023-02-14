196964ca9464036c3489f08f52b4e384346bad6b
dosda13u <dosda13u@etu.univ-lorraine.fr>
2023-02-14 21:36:04 +0100

    Correction de certaines fonctions

--- src/list.c
+++ src/list.c
@@ -23,6 +23,7 @@ static struct list_node_t * new_list_node(void * data) {
 	newListNode->data = data;
 	newListNode->successor = NULL;
 	newListNode->predecessor = NULL;
+	return newListNode;
 }
 
 void * get_list_node_data(const struct list_node_t * node) {
@@ -89,6 +90,9 @@ void increase_list_size(struct list_t * L) {
 }
 
 void decrease_list_size(struct list_t * L) {
+	if (list_is_empty(L) == 1) {
+		ShowMessage("src.list.c:decrease_list_size : La liste est vide, inutile de la decrementer !", 0);
+	}
 	L->numelm --;
 }
 
@@ -97,34 +101,53 @@ void set_list_size(struct list_t * L, int newSize) {
 }
 
 void set_head(struct list_t * L, struct list_node_t * newHead) {
-	L->head = newHead;
+	if (list_is_empty(L) == 1){
+		// On ne fait rien car la liste est vide et donc la tête est NULL
+		// Pour pouvoir mettre la tête à newHead, il faut creer un noeud puis l'inserer dans la liste avec list_insert_first, list_insert_last ou list_insert_after
+		ShowMessage("src.list.c:set_head : La liste est vide !", 0);
+	} 
+	else {
+		L->head = newHead;
+	}
+	
 }
 
 void set_tail(struct list_t * L, struct list_node_t * newTail) {
-	L->tail = newTail;
+	if (list_is_empty(L) == 1){
+		// Meme raisonnement que pour set_head
+		ShowMessage("src.list.c:set_tail : La liste est vide !", 0);
+	} 
+	else {
+		L->tail = newTail;
+	}
 }
 
 void delete_list(struct list_t * L, int deleteData) {
-	if (deleteData == 1){
-		while (L->head != NULL){
-			struct list_node_t * temp = L->head;
-			L->head = L->head->successor;
-			L->freeData(temp->data);
-			free(temp);
-		}
+	if (list_is_empty(L) == 1){
+		ShowMessage("src.list.c:delete_list : La liste est déjà vide !", 0);
 	}
 	else {
-		if (deleteData == 0){
+		if (deleteData == 1){
 			while (L->head != NULL){
 				struct list_node_t * temp = L->head;
 				L->head = L->head->successor;
+				L->freeData(temp->data);
 				free(temp);
-				}
 			}
-		else {
-			ShowMessage("Erreur src.list.c:delete_list , deleteData doit être égal à 0 ou 1  ", 1);
 		}
-		
+		else {
+			if (deleteData == 0){
+				while (L->head != NULL){
+					struct list_node_t * temp = L->head;
+					L->head = L->head->successor;
+					free(temp);
+					}
+				}
+			else {
+				ShowMessage("Erreur src.list.c:delete_list , deleteData doit être égal à 0 ou 1  ", 1);
+			}
+			
+		}
 	}
 }
 
@@ -138,11 +161,11 @@ void list_insert_first(struct list_t * L, void * data) {
 	struct list_node_t * temp = new_list_node(data);
 
 	if(list_is_empty(L)){
-		L->head = temp;
-		L->tail = temp;
+		set_head(L, temp);
+		set_tail(L, temp);
 	}else{
-		temp->successor = L->head;
-		L->head = temp;
+		set_successor(temp, get_list_head(L));
+		set_head(L, temp);
 	}
 	L->numelm++;
 }
@@ -150,7 +173,7 @@ void list_insert_first(struct list_t * L, void * data) {
 void list_insert_last(struct list_t * L, void * data) {
 	struct list_node_t * newListNode = new_list_node(data);
 	if (list_is_empty(L)) {
-		L->head = newListNode;
+		set_head(L, newListNode);
 		L->tail = newListNode;
 	}
 	else {
